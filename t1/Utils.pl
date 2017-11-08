0fffa087b9fea2f4d7fed9f620ebd19116d4b479
David Falcão <up201506571@fe.up.pt>
2017-11-08 21:55:40 +0000

    + direction_mov
    predicado que determina a direção do movimento

--- t1/Utils.pl
+++ t1/Utils.pl
@@ -12,3 +12,13 @@ newLine(_,_).
 
 ascii_to_dec(N,N1):-
 	N1 is N-48.
+
+direction_mov(0,0,'null').
+direction_mov(1,0,'n').
+direction_mov(-1,0,'s').
+direction_mov(0,1,'e').
+direction_mov(0,-1,'w').
+direction_mov(1,1,'ne').
+direction_mov(1,-1,'nw').
+direction_mov(-1,1,'se').
+direction_mov(-1,-1,'sw').
